525483aad56fddf6fb145de4d5338e6f72ee3d4b
cacaudev <cacauabreu2013@gmail.com>
2018-02-26 17:38:20 -0300

    Add POST /todos route and refactor mongoose

--- server/server.js
+++ server/server.js
@@ -1,61 +1,27 @@
-const mongoose = require('mongoose');
+const express    = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+const {mongoose} = require('./db/mongoose');
+const {Todo}     = require('./models/todo');
+const {User}     = require('./models/user');
 
-let Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.post('/todos', (req, res) =>{
+  let todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc); // 200 - OK
+  }).catch((error) => {
+    res.sendStatus(400); // 400 - Bad request
+  });
 });
 
-let User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1
-  }
-});
-
-// let newTodo = new Todo({
-//   text: 'Cook dinner'
-// });
-
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo', doc);
-// }, (error) => {
-//   console.log('Unable to save todo', error);
-// });
-
-// let otherTodo = new Todo({
-//   text: '     Eat lunch       '
-// });
-
-// otherTodo.save().then((doc) => {
-//   console.log(JSON.stringify(doc, undefined, 2));
-// }, (error) => {
-//   console.log('Unable to save todo', error);
-// });
-
-let newUser = new User({
-  email: ' cacau@test.br'
-});
-
-newUser.save().then((doc) => {
-  console.log('User saved:');
-  console.log(JSON.stringify(doc, undefined, 2));
-}, (error) => {
-  console.log('Unable to save user', error);
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
