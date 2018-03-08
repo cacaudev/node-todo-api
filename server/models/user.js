e87740ca9acd86e3beece2ea0fa42ad7de42b91f
cacaudev <cacauabreu2013@gmail.com>
2018-03-07 21:17:25 -0300

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -93,5 +93,21 @@ UserSchema.pre('save', function (next) {
   }
 });
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  let User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) return Promise.reject();
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result) resolve(user);
+        else reject();
+      });
+    });
+  });
+  
+};
+
 let User = mongoose.model('User', UserSchema);
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const express    = require('express');
 const bodyParser = require('body-parser');
 const app        = express();
 const {ObjectID} = require('mongodb');
+const bcrypt     = require('bcryptjs');
 
 const {mongoose}     = require('./db/mongoose');
 const {Todo}         = require('./models/todo');
@@ -94,6 +95,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+   }).catch((err) => {
+    res.status(400).send(err);
+  });  
 });
 
 app.listen(port, () => {
